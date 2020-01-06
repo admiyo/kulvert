3163aee4b13123705d10681939b23095b0de8c55
Adam Young <ayoung@redhat.com>
2020-01-06 17:23:26 -0500

    Added a root page

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, web, App,  HttpRequest, HttpServer, Responder};
+
+async fn root(req: HttpRequest) -> &'static str {
+    println!("REQ: {:?}", req);
+    "Hello world!"
+}
 
 #[get("/{id}/{name}/index.html")]
 async fn index(info: web::Path<(u32, String)>) -> impl Responder {
@@ -7,7 +12,11 @@ async fn index(info: web::Path<(u32, String)>) -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(web::resource("/").to(root))
+    })
         .bind("127.0.0.1:8080")?
         .run()
         .await
