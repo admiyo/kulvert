e851186442d424888e6bc66a8a2e9f249a326726
Adam Young <ayoung@redhat.com>
2020-01-08 14:45:59 -0500

    Extracted some hostname strings into their own functions

--- src/versions.rs
+++ src/versions.rs
@@ -1,6 +1,5 @@
 use serde::Serialize;
 
-
 #[derive(Clone, Serialize)]
 pub struct Link {
     href: String,
@@ -29,23 +28,65 @@ pub struct Versions {
 }
 
 
+fn get_base_url() -> String {
+    return "http://localhost:8080".to_string();
+}
+
+fn get_v3_url() -> String {
+    format!("{}{}", get_base_url(), "/v3")
+}
+
+
 
 fn get_json_media_type() -> [MediaType; 1] {
-    let a = MediaType { base: "application/json".to_string(),
-                         media_type: "application/vnd.openstack.identity-v3+json".to_string()
+    let a = MediaType {
+        base: "application/json".to_string(),
+        media_type: "application/vnd.openstack.identity-v3+json".to_string()
     };
 
     return [a];
 }
 
-fn get_v3_links() -> [Link; 1]{
+fn get_v3_summary_links() -> [Link; 1]{
     let a = Link{
-        href: "http://localhost:8080/v3/".to_string(),
+        href: get_v3_url().to_string(),
         htype: "text/html".to_string(),
         rel: "self".to_string()
     };
     return [a];
 }
+
+
+fn get_v3_links() -> [Link; 2]{
+    let selflink = Link{
+        href: get_v3_url().to_string(),
+        htype: "text/html".to_string(),
+        rel: "self".to_string()
+    };
+
+
+    let userslink = Link{
+        href: "http://localhost:8080/v3/users".to_string(),
+        htype: "text/html".to_string(),
+        rel: "users".to_string()
+    };
+
+    return [selflink, userslink];
+}
+
+
+pub fn get_v3_summary() -> Version{
+    let a = Version {
+        status: "stable".to_string(),
+        updated: "2018-02-28T00:00:00Z".to_string(),
+        media_types: get_json_media_type().to_vec(),
+        id: "v3.10".to_string(),
+        links: get_v3_summary_links().to_vec()
+            
+    };
+    return a;
+}
+
 
 pub fn get_v3() -> Version{
     let a = Version {
@@ -62,33 +103,9 @@ pub fn get_v3() -> Version{
 
 pub fn get_versions() -> Versions{
     let a = Versions{
-        versions: [get_v3()].to_vec()
+        versions: [get_v3_summary()].to_vec()
     };
     return a;
     
 }
 
-/*
-{"versions":
- {"values": [{"status": "stable",
-              "updated": "2018-02-28T00:00:00Z",
-              "media-types": [
-                  {"base": "application/json",
-                   "type": "application/vnd.openstack.identity-v3+json"}],
-              "id": "v3.10", "links": [
-                  {"href": "https://kaizen.massopen.cloud:13000/v3/",
-                   "rel": "self"}
-              ]},
-             {"status": "deprecated",
-              "updated": "2016-08-04T00:00:00Z",
-              "media-types": [
-                  {"base": "application/json",
-                   "type": "application/vnd.openstack.identity-v2.0+json"}],
-              "id": "v2.0",
-              "links": [
-                  {"href": "https://kaizen.massopen.cloud:13000/v2.0/",
-                   "rel": "self"},
-                  {"href": "https://docs.openstack.org/",
-                   "type": "text/html", "rel": "describedby"}]}
- ]}
-}*/
