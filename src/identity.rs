0c646e1a4e59bdeef44e4e3dfe4ac6fe4cbf7a47
Adam Young <ayoung@redhat.com>
2020-02-10 11:49:34 -0500

    added placeholders for subordinate entites in identity

--- src/identity.rs
+++ src/identity.rs
@@ -4,7 +4,9 @@ use serde::Serialize;
 #[derive(Clone, Serialize)]
 pub struct Providers {
     providers: Vec<Provider>,
-
+    protocols: Vec<Protocol>,
+    mappings: Vec<Mapping>,
+    accounts: Vec<Account>
 }
 
 #[derive(Clone, Serialize)]
@@ -15,10 +17,45 @@ pub struct Provider {
     enabled: bool,
 }
 
+#[derive(Clone, Serialize)]
+pub struct Protocol {
+    id: String,
+    name: String,
+    provider: String,
+    enabled: bool,
+}
+
+#[derive(Clone, Serialize)]
+pub struct Mapping {
+    id: String,
+    name: String,
+    provider: String,
+    enabled: bool,
+}
+
+#[derive(Clone, Serialize)]
+pub struct Account {
+    id: String,
+    name: String,
+    provider: String,
+    enabled: bool,
+}
+
+#[derive(Clone, Serialize)]
+pub struct Login {
+    id: String,
+    name: String,
+    provider: String,
+    enabled: bool,
+}
+
 
 pub fn get_providers() -> Providers{
     let a = Providers {
-        providers : [].to_vec()
+        providers : [].to_vec(),
+        protocols: [].to_vec(),
+        mappings: [].to_vec(),
+        accounts: [].to_vec(),
     };
     return a;
 }
